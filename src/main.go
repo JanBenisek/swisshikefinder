1264c5e16c9fff843e656d91c001fa0f2f20c628
Jan Benisek <benisekj@gmail.com>
2024-05-04 20:54:44 +0200

    Fix: back to old settings with port (#20)

--- src/main.go
+++ src/main.go
@@ -51,8 +51,13 @@ func main() {
 	// Get the application struct and set some env values
 	app := conf.AppLog
 
-	// Env var? Digital Ocean always listens on 8080
-	app.Port = ":8080"                      // will be available at http://localhost:8080
+	// Digital Ocean always listens on 8080 and has the env var set
+	app.Port = os.Getenv("PORT") // will be available at http://localhost:8080
+	if app.Port == "" {
+		app.InfoLog.Printf("Port not found in .env, using default 8080")
+		app.Port = "8080"
+	}
+
 	app.API_key = os.Getenv("HIKE_API_KEY") // maybe get rid of it?
 	if app.API_key == "" {
 		app.ErrorLog.Fatal("Env: apiKey must be set")
@@ -67,12 +72,12 @@ func main() {
 
 	// my version of server, I can pass my own logger
 	srv := &http.Server{
-		Addr:     app.Port,
+		Addr:     ":" + app.Port,
 		ErrorLog: app.ErrorLog,
 		Handler:  routes(app), // giving it my routes
 	}
 
-	app.InfoLog.Printf("Starting server on %s", app.Port)
+	app.InfoLog.Printf("Starting server on %s", srv.Addr)
 	srv.ListenAndServe() //start the service and listen to the port with the mux
 
 }
